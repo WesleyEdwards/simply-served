130650742b5c2d12ace022655b6b1e237571a397
WesleyEdwards <westheedwards@gmail.com>
2025-03-28 22:37:27 -0600

    model rest test

--- tests/mocks/server.ts
+++ tests/mocks/server.ts
@@ -1,11 +1,11 @@
 import {z} from "zod"
 import express from "express"
 import {
-  bearerTokenAuth,
   buildRoute,
   createControllers,
-  createRoutes,
   createSimplyServer,
+  modelRestEndpoints,
+  UnauthorizedError,
 } from "../../src"
 import todoDb from "./database"
 import {TodoDb} from "./types"
@@ -20,22 +20,28 @@ export type MockCtx = {
 export const getMockServer = () => {
   const mockApp = express()
   mockApp.use(express.json())
-
   const server = createSimplyServer<MockCtx>({
     initContext: {db: todoDb},
-    getAuth: bearerTokenAuth("supersecretencryptionkey"),
+    getAuth: (req) => {
+      const userId = req.headers.authorization?.split(" ")?.at(1)
+      if (userId) {
+        if (!userId) {
+          throw new UnauthorizedError()
+        }
+        if (userId.length !== 36) {
+          throw new UnauthorizedError("Invalid userid")
+        }
+        return {userId: userId}
+      }
+      return undefined
+    },
     controllers: createControllers(({createController}) => [
       createController({
         path: "/todo",
-        routes: createRoutes(({createModelRestEndpoints}) => [
-          ...createModelRestEndpoints({
+        routes: [
+          ...modelRestEndpoints<MockCtx, TodoType>({
             collection: (db) => db.todo,
-            validator: z.object({
-              _id: z.string().uuid(),
-              todoItem: z.string(),
-              owner: z.string().uuid(),
-              done: z.boolean().default(true),
-            }),
+            validator: todoSchema,
             permissions: {
               read: {type: "publicAccess"},
               create: {type: "publicAccess"},
@@ -61,19 +67,14 @@ export const getMockServer = () => {
               body
               throw new Error("")
             }),
-        ]),
+        ],
       }),
       createController({
         path: "/user",
-        routes: createRoutes(({createModelRestEndpoints}) => [
-          ...createModelRestEndpoints({
+        routes: [
+          ...modelRestEndpoints<MockCtx, UserType>({
             collection: (db) => db.todo,
-            validator: z.object({
-              _id: z.string().uuid(),
-              todoItem: z.string(),
-              owner: z.string().uuid(),
-              done: z.boolean().default(true),
-            }),
+            validator: userSchema,
             permissions: {
               read: {type: "publicAccess"},
               create: {type: "publicAccess"},
@@ -84,7 +85,7 @@ export const getMockServer = () => {
               },
             },
           }),
-        ]),
+        ],
       }),
     ]),
     afterGenerateEndpoints: (app) => {
@@ -98,3 +99,19 @@ export const getMockServer = () => {
 
   return mockApp
 }
+
+export type TodoType = z.infer<typeof todoSchema>
+
+const todoSchema = z.object({
+  _id: z.string().uuid(),
+  todoItem: z.string(),
+  owner: z.string().uuid(),
+  done: z.boolean().default(true),
+})
+const userSchema = z.object({
+  _id: z.string().uuid(),
+  todoItem: z.string(),
+  owner: z.string().uuid(),
+  done: z.boolean().default(true),
+})
+type UserType = z.infer<typeof userSchema>
